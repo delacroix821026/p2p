a922d9af4239efd0479b1705440ae67828e4c7d9
岱隽岑 <66039496@qq.com>
2017-09-11 17:30:34 +0800

    no message

---
+++ p2p_zuul/src/main/java/com/newland/financial/p2p/Application.java
@@ -0,0 +1,13 @@
+package com.newland.financial.p2p;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
