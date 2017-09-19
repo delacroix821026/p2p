8ecee6d073929e78d4750a5d8774bda7f34a2135
岱隽岑 <66039496@qq.com>
2017-09-19 11:05:54 +0800

    no message

--- p2p_admin/src/main/java/com/newland/financial/p2p/Application.java
+++ p2p_admin/src/main/java/com/newland/financial/p2p/Application.java
@@ -1,12 +1,12 @@
 package com.newland.financial.p2p;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableAdminServer
 @EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
