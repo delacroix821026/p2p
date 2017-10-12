b7ec8ba139b0a9d64b9c782eaec54ebc4ca83f65
xiazunhua <805791360@qq.com>
2017-10-12 10:22:40 +0800

    123

--- p2p_server/domain/src/main/java/com/newland/financial/p2p/Application.java
+++ p2p_server/domain/src/main/java/com/newland/financial/p2p/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author cendaijuan
@@ -14,6 +15,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties
+@ImportResource(value = "classpath*:datasource-spring.xml")
 @Log
 public class Application {
     /**
