d48415d4d5b8e053dcc304155004d6ed2e32b2b4
xiazunhua <805791360@qq.com>
2017-10-14 11:18:22 +0800

    12121

--- p2p_server/service/src/test/java/integration/ProductServiceTest.java
+++ p2p_server/service/src/test/java/integration/ProductServiceTest.java
@@ -2,7 +2,6 @@ package integration;
 
 import com.newland.financial.p2p.Application;
 import com.newland.financial.p2p.dao.IInterestDao;
-import com.newland.financial.p2p.dao.IOrgNegativeDao;
 import com.newland.financial.p2p.dao.IOrganizationDao;
 import com.newland.financial.p2p.dao.IProductDao;
 import com.newland.financial.p2p.domain.entity.Interest;
@@ -32,8 +31,6 @@ public class ProductServiceTest {
     @Autowired
     private IInterestDao iInterestDao;
     @Autowired
-    private IOrgNegativeDao orgNegativeDao;
-    @Autowired
     private IOrganizationDao organizationDao;
     @Before
     public void setUpClass() {
@@ -41,7 +38,7 @@ public class ProductServiceTest {
     }
     @Test
     public void A_insertProduct(){
-        String jsonStr = "{\"proId\":\"cs001\",\"proName\":\"banana\",\"proLmt\":10000,\"interestList\":[{\"times\":3},{\"times\":6}],\"proNameOperator\":\"banana\",\"sponsor\":\"banana\",\"sprProName\":\"banana\",\"maxLmt\":100000,\"role\":\"1\",\"orgs\":[{\"organization\":\"cs007\"},{\"organization\":\"cs008\"}],\"repayMhd\":\"1\",\"interestMhd\":\"1\",\"cutMhd\":\"1\",\"advanceRepay\":\"1\",\"poundage\":\"1\",\"formula\":\"5+1=6\",\"isLatefee\":\"1\",\"latefee\":20,\"positiveOrNegative\":\"1\"}";
+        String jsonStr = "{\"proId\":\"cs001\",\"proName\":\"banana\",\"proLmt\":10000,\"interestList\":[{\"times\":3},{\"times\":6}],\"proNameOperator\":\"banana\",\"sponsor\":\"banana\",\"sprProName\":\"banana\",\"maxLmt\":100000,\"role\":\"1\",\"orgs\":[{\"organization\":\"cs007\"},{\"organization\":\"cs008\"}],\"repayMhd\":\"1\",\"interestMhd\":\"1\",\"cutMhd\":\"1\",\"advanceRepay\":\"1\",\"poundage\":\"1\",\"formula\":\"5+1=6\",\"isLatefee\":\"1\",\"latefee\":20}";
         productService.insertProduct(jsonStr);
         Product product = (Product) productService.getProduct("cs001");
         List<Interest> list = product.getInterestList();
@@ -64,12 +61,6 @@ public class ProductServiceTest {
         Assert.assertEquals("formula not same","5+1=6",product.getFormula());
         Assert.assertEquals("isLatefee not same","1",product.getIsLatefee());
         Assert.assertEquals("latefee not same",new BigDecimal("20.00"),product.getLatefee());
-        Assert.assertEquals("positiveOrNegative not same","1",product.getPositiveOrNegative());
-
-        String jsonStr1 = "{\"proId\":\"cs002\",\"proName\":\"ppppp\",\"proLmt\":10000,\"interestList\":[{\"times\":3},{\"times\":6}],\"proNameOperator\":\"ppppp\",\"sponsor\":\"ppppp\",\"sprProName\":\"ppppp\",\"maxLmt\":100000,\"role\":\"1\",\"orgs\":[{\"organization\":\"cs0071\"},{\"organization\":\"cs0081\"}],\"repayMhd\":\"1\",\"interestMhd\":\"1\",\"cutMhd\":\"1\",\"advanceRepay\":\"1\",\"poundage\":\"1\",\"formula\":\"5+1=6\",\"isLatefee\":\"1\",\"latefee\":20,\"positiveOrNegative\":\"2\"}";
-        productService.insertProduct(jsonStr1);
-        Product product1 = (Product) productService.getProduct("cs002");
-        Assert.assertEquals("positiveOrNegative not same","2",product1.getPositiveOrNegative());
     }
 
     @Test
@@ -86,8 +77,6 @@ public class ProductServiceTest {
         productDao.deleteProduct("cs002");
         iInterestDao.deleteInterestByProId("cs001");
         iInterestDao.deleteInterestByProId("cs002");
-        orgNegativeDao.deleteOrgNegative("cs001");
-        orgNegativeDao.deleteOrgNegative("cs002");
         organizationDao.deleteOrganization("cs001");
         organizationDao.deleteOrganization("cs002");
     }
