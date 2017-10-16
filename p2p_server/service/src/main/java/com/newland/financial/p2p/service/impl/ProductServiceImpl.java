20e0102eb30c2c00784615fbc3c862890e05c166
xiazunhua <805791360@qq.com>
2017-10-16 09:39:07 +0800

    123

--- p2p_server/service/src/main/java/com/newland/financial/p2p/service/impl/ProductServiceImpl.java
+++ p2p_server/service/src/main/java/com/newland/financial/p2p/service/impl/ProductServiceImpl.java
@@ -139,7 +139,7 @@ public class ProductServiceImpl implements IProductService {
         b2 = organizationDao.insertOrganizationList(list1); //将该产品对应可查看到的机构插入机构表中
         Boolean b1 = interestDao.insertInterest(list); //将产品各分期利率插入利率表中
         Boolean b3 = productDao.insertProduct(product); //将产品信息插入产品表中
-        logger.info("机构插入：" + b2 + ",分期插入：" + b1 + ",产品插入：" + b3);
+        logger.info("机构插入b2---：" + b2 + ",分期插入b1---：" + b1 + ",产品插入b3----：" + b3);
         if (b1 && b2 && b3) {
             return true;
         } else {
