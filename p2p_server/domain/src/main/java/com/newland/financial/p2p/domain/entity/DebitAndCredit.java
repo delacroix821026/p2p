274b8fb7e1f7e82ead9f7e062e218ee36400e9c0
xiazunhua <805791360@qq.com>
2017-10-20 09:56:00 +0800

    贷款单增加放款日期字段

--- p2p_server/domain/src/main/java/com/newland/financial/p2p/domain/entity/DebitAndCredit.java
+++ p2p_server/domain/src/main/java/com/newland/financial/p2p/domain/entity/DebitAndCredit.java
@@ -71,4 +71,6 @@ public class DebitAndCredit extends BaseEntity {
     private Date lastRePayDate;
     /**0已结清显示我的贷款，1申请中，2还款计划，3拒绝显示我的贷款.*/
     private String stus;
+     /**放款日期.*/
+     private Date loanDate;
 }
