1c7303e315a1914df62b63a685af7bc5c2fb990a
gregory-zhy <2275515818@qq.com>
2017-10-23 12:12:45 +0800

    Config
    Config

--- p2p_schedule/service/src/main/java/com/newland/financial/p2p/service/impl/TransToSmallLoanConfig.java
+++ p2p_schedule/service/src/main/java/com/newland/financial/p2p/service/impl/TransToSmallLoanConfig.java
@@ -1,9 +1,11 @@
 package com.newland.financial.p2p.service.impl;
 
 import com.google.common.collect.Lists;
+import com.newland.financial.p2p.domain.entity.BaseEntity;
+import com.newland.financial.p2p.domain.entity.CustomerFlowDebit;
 import com.newland.financial.p2p.domain.entity.DebitAndCredit;
+import com.newland.financial.p2p.domain.entity.ExcelOrderModel;
 import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -11,9 +13,7 @@ import org.mybatis.spring.batch.MyBatisBatchItemWriter;
 import org.mybatis.spring.batch.MyBatisPagingItemReader;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
@@ -24,7 +24,6 @@ import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.support.CompositeItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -35,7 +34,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @Configuration
-@Log4j
+@Log
 public class TransToSmallLoanConfig {
     @Autowired
     private JobBuilderFactory jobs;
@@ -47,38 +46,35 @@ public class TransToSmallLoanConfig {
     private JobLauncher jobLauncher;
 
     @Bean
-    @StepScope
-    //@Scope("prototype")
+    @Scope("prototype")
     public ItemReader getItemReader(SqlSessionFactory sqlSessionFactory) {
         MyBatisPagingItemReader myBatisPagingItemReader = new MyBatisPagingItemReader();
         myBatisPagingItemReader.setSqlSessionFactory(sqlSessionFactory);
         myBatisPagingItemReader.setPageSize(10);
-        myBatisPagingItemReader.setQueryId("selectByStus");
+        myBatisPagingItemReader.setQueryId("selectSendOrderList");
         return myBatisPagingItemReader;
     }
 
     @Bean
-    @StepScope
-    //@Scope("prototype")
+    @Scope("prototype")
     public ItemWriter getItemWriter(SqlSessionFactory sqlSessionFactory) {
         MyBatisBatchItemWriter myBatisBatchItemWriter = new MyBatisBatchItemWriter();
         myBatisBatchItemWriter.setSqlSessionFactory(sqlSessionFactory);
         myBatisBatchItemWriter.setSqlSessionTemplate(new SqlSessionTemplate(sqlSessionFactory, ExecutorType.BATCH));
-        myBatisBatchItemWriter.setStatementId("updateStus");
+        myBatisBatchItemWriter.setStatementId("updateOrderSendStus");
         return myBatisBatchItemWriter;
     }
 
     @Bean
-    @StepScope
-    //@Scope("prototype")
-    public ItemWriter getFlatFileItemWriter(@Value("#{jobParameters['runDay']}") String runDay) throws URISyntaxException, MalformedURLException {
-        log.info("runDay:" + "/Users/daijuancen/Desktop/outputfile-" + runDay + ".csv");
+    @Scope("prototype")
+    public ItemWriter getFlatFileItemWriter() throws URISyntaxException, MalformedURLException {
         FlatFileItemWriter flatFileItemWriter = new FlatFileItemWriter();
-        flatFileItemWriter.setResource(new PathResource("/Users/daijuancen/Desktop/outputfile-" + runDay + ".csv"));
+        flatFileItemWriter.setResource(new PathResource("d:\\scheduel\\outputfile-#{jobParameters['runDay']}.csv"));
         DelimitedLineAggregator delimitedLineAggregator = new DelimitedLineAggregator();
         delimitedLineAggregator.setDelimiter(",");
         BeanWrapperFieldExtractor beanWrapperFieldExtractor = new BeanWrapperFieldExtractor();
-        beanWrapperFieldExtractor.setNames(new String[] {"dtId", "dIttId", "dProName"});
+        beanWrapperFieldExtractor.setNames(new String[] {"dDate","oddNumbers", "applyName","phone","dMoney",
+                "detailAdd","identityCard","corpPhone","merchantName"});
         delimitedLineAggregator.setFieldExtractor(beanWrapperFieldExtractor);
         flatFileItemWriter.setLineAggregator(delimitedLineAggregator);
 
@@ -86,20 +82,16 @@ public class TransToSmallLoanConfig {
     }
 
     @Bean
-    //@Scope("prototype")
     public CreateExcelProcessor getCreateExcelProcessor() {
         return new CreateExcelProcessor();
     }
 
     @Bean
-    //@Scope("prototype")
     public TransforToFtpTasklet getTransforToFtpTasklet() {
         return new TransforToFtpTasklet();
     }
 
     @Bean(name= "createTransToSmallLoanCompanyJob")
-    //@JobScope
-    //@Scope("prototype")
     public Job createTransforToSmallLoanCompanyJob(@Qualifier("step1") Step step1, @Qualifier("step2") Step step2) {
         Job job = jobs.get("transforToSmallLoanCompany")
                 .incrementer(new RunIdIncrementer())
@@ -110,18 +102,18 @@ public class TransToSmallLoanConfig {
     }
 
     @Bean
-    protected Step step1(@Qualifier("getItemReader") ItemReader<DebitAndCredit> reader,
+    protected Step step1(@Qualifier("getItemReader") ItemReader<CustomerFlowDebit> reader,
                          CreateExcelProcessor createExcelProcessor,
-                         @Qualifier("getItemWriter") ItemWriter<DebitAndCredit> dbWriter,
-                         @Qualifier("getFlatFileItemWriter") ItemWriter<DebitAndCredit> fileWriter) {
-        CompositeItemWriter<DebitAndCredit> writer = new CompositeItemWriter<DebitAndCredit>();
+                         @Qualifier("getItemWriter") ItemWriter<CustomerFlowDebit> dbWriter,
+                         @Qualifier("getFlatFileItemWriter") ItemWriter<ExcelOrderModel> fileWriter) {
+        CompositeItemWriter<BaseEntity> writer = new CompositeItemWriter<BaseEntity>();
         List delegates = Lists.newArrayList();
         delegates.add(dbWriter);
         delegates.add(fileWriter);
         writer.setDelegates(delegates);
 
         return steps.get("step1")
-                .<DebitAndCredit, DebitAndCredit> chunk(5)
+                .<CustomerFlowDebit, ExcelOrderModel> chunk(5)
                 .reader(reader)
                 .processor(createExcelProcessor)
                 .writer(writer)
