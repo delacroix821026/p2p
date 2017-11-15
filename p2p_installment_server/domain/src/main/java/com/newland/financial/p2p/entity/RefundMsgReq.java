282b711c277bac5fe652a0ea2aff9fa13ab2e819
xiazunhua <805791360@qq.com>
2017-11-15 15:51:10 +0800

    1111111

---
+++ p2p_installment_server/domain/src/main/java/com/newland/financial/p2p/entity/RefundMsgReq.java
@@ -0,0 +1,17 @@
+package com.newland.financial.p2p.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Mxia
+ * 退款请求报文.
+ */
+@Setter
+@Getter
+public class RefundMsgReq extends BaseEntityReq {
+    /**合同号.*/
+    private String contractsCode;
+    /**异步通知地址.*/
+    private String backUrl;
+}
