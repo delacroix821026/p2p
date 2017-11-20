ff7fd5ea6365a6ba44337ebb2324ec2a3429ded0
xiazunhua <805791360@qq.com>
2017-11-20 13:48:14 +0800

    1212

--- p2p_installment_client/service/src/main/java/com/newland/financial/p2p/service/ISendService.java
+++ p2p_installment_client/service/src/main/java/com/newland/financial/p2p/service/ISendService.java
@@ -1,13 +1,12 @@
 package com.newland.financial.p2p.service;
 
-import com.newland.financial.p2p.service.Impl.SendServiceHystrix;
-import org.omg.CORBA.OBJ_ADAPTER;
+import com.newland.financial.p2p.service.Impl.SendServiceFallBackFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "p2p",url = "localhost:3003", fallback = SendServiceHystrix.class)
+@FeignClient(name = "p2p",url = "localhost:3003", fallbackFactory = SendServiceFallBackFactory.class)
 public interface ISendService {
     @RequestMapping(method = RequestMethod.POST, value = "/ybforder/sendOrderMsg")
     Object sendOrderMsgToLbf(@RequestBody Object object);
---
+++ p2p_installment_client/service/src/main/java/com/newland/financial/p2p/service/Impl/OrderServiceFallBackFactory.java
@@ -0,0 +1,39 @@
+package com.newland.financial.p2p.service.Impl;
+
+import com.newland.financial.p2p.service.IOrderService;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+
+@Log4j
+@Component
+public class OrderServiceFallBackFactory implements FallbackFactory<IOrderService> {
+    public IOrderService create(final Throwable cause) {
+        return new IOrderService() {
+            public String createOrderInfo(String jsonStr) {
+                log.info("*********createOrderInfo:被熔断***********");
+                return "1026";
+            }
+
+            public Object findOrderInfo(String jsonStr) {
+                log.info("*********findOrderInfo:被熔断***********");
+                return "1026";
+            }
+
+            public Object tradeUpdateOrder(String jsonStr) {
+                log.info("*********tradeUpdateOrder:被熔断***********");
+                return "1026";
+            }
+
+            public Object updateOrderInfo(Object ob) {
+                log.info("*********updateOrderInfo:被熔断***********");
+                return "1026";
+            }
+
+            public Object updateAndGetOrder(Object ob) {
+                log.info("*********updateAndGetOrder:被熔断***********");
+                return "1026";
+            }
+        };
+    }
+}
